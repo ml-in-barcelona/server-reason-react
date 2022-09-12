2473513b6c3cb5d4e147196a39319e6fad603634
Javier Chávarri <javier.chavarri@gmail.com>
2022-09-12 16:53:02 +0200

    cleanup

--- example/src/static-requires.js
+++
@@ -1,4 +0,0 @@
-var primitive = require("./static/primitive.css");
-var prismJs = require("./static/prism.js");
-var prismCss = require("./static/prism.css");
-var leafy = require("./static/leafy.js");
