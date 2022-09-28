d78aed8b63a324cb0d9238127284d291d4cbb044
David Sancho Moreno <dsnxmoreno@gmail.com>
2022-09-28 16:50:51 +0200

    Add ReasonReactRouter

--- lib/reasonReactRouter.ml
+++ lib/reasonReactRouter.ml
@@ -1,1 +1,28 @@
-let push _str = ()
+let hash _location = ""
+
+(* TODO: Maybe this should be implemented? *)
+let path ?serverUrlString:_ () = []
+
+(* TODO: Maybe this should be implemented? *)
+let search ?serverUrlString:_ () = ""
+let push _path = ()
+let replace _path = ()
+
+type url =
+  { path : string list
+  ; hash : string
+  ; search : string
+  }
+
+type watcherID = unit -> unit
+
+let url ?serverUrlString () =
+  { path = path ?serverUrlString ()
+  ; hash = hash ()
+  ; search = search ?serverUrlString ()
+  }
+
+let dangerouslyGetInitialUrl = url
+let watchUrl _callback () = ()
+let unwatchUrl _watcherID = ()
+let useUrl ?serverUrl:_ () = url ()
