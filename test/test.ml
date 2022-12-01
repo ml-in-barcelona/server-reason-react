339291c7aaba995aabc6ddb2fafd5c5661db84c0
David Sancho Moreno <dsnxmoreno@gmail.com>
2022-12-01 13:10:17 +0100

    Rename test file names

--- test/test.ml
+++ test/test.ml
@@ -6,6 +6,6 @@ let () =
     ; Test_renderToString.tests
     ; Test_cloneElement.tests
     ; Test_reactDOMStyle.tests
-    ; Test_emotion_hash.tests
-    ; Test_emotion_styles.tests
+    ; Test_css_hash.tests
+    ; Test_css_styles.tests
     ]
