a916d60e0748b7228e2c40fc54bf0f43b50fae0d
David Sancho Moreno <dsnxmoreno@gmail.com>
2023-04-04 11:21:53 +0200

    Add svg test

--- ppx/test/test.re
+++ ppx/test/test.re
@@ -194,6 +194,21 @@ let onclick_inline_string = () => {
   );
 };
 
+let svg = () => {
+  assert_string(
+    ReactDOM.renderToStaticMarkup(<svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="24px"
+      height="24px">
+      <path
+        d="M 5 3 C 3.9069372 3 3 3.9069372 3 5 L 3 19 C 3 20.093063 3.9069372 21 5 21 L 19 21 C 20.093063 21 21 20.093063 21 19 L 21 12 L 19 12 L 19 19 L 5 19 L 5 5 L 12 5 L 12 3 L 5 3 z M 14 3 L 14 5 L 17.585938 5 L 8.2929688 14.292969 L 9.7070312 15.707031 L 19 6.4140625 L 19 10 L 21 10 L 21 3 L 14 3 z"
+      />
+    </svg>),
+    "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\" width=\"24px\" height=\"24px\"><path d=\"M 5 3 C 3.9069372 3 3 3.9069372 3 5 L 3 19 C 3 20.093063 3.9069372 21 5 21 L 19 21 C 20.093063 21 21 20.093063 21 19 L 21 12 L 19 12 L 19 19 L 5 19 L 5 5 L 12 5 L 12 3 L 5 3 z M 14 3 L 14 5 L 17.585938 5 L 8.2929688 14.292969 L 9.7070312 15.707031 L 19 6.4140625 L 19 10 L 21 10 L 21 3 L 14 3 z\"></path></svg>",
+  );
+};
+
 let case = (title, fn) => Alcotest.test_case(title, `Quick, fn);
 
 let _ =
@@ -209,6 +224,7 @@ let _ =
           case("input_bool_attrs", bool_attributes),
           case("p_inner_html", innerhtml),
           case("div_int_attr", int_attribute),
+          case("svg", svg),
           case("style_attr", style_attribute),
           case("div_ref_attr", ref_attribute),
           case("link_as_attr", link_as_attribute),
--- test/test_renderToStaticMarkup.ml
+++ test/test_renderToStaticMarkup.ml
@@ -249,6 +249,46 @@ let render_with_doc_type () =
     (ReactDOM.renderToStaticMarkup ~docType:ReactDOM.HTML5 div)
     "<!DOCTYPE html><div><span>This is valid HTML5</span></div>"
 
+(* <svg
+         xmlns="http://www.w3.org/2000/svg"
+         viewBox="0 0 24 24"
+         width="24px"
+         height="24px">
+         <path
+           d="M 5 3 C 3.9069372 3 3 3.9069372 3 5 L 3 19 C 3 20.093063 3.9069372 21 5 21 L 19 21 C 20.093063 21 21 20.093063 21 19 L 21 12 L 19 12 L 19 19 L 5 19 L 5 5 L 12 5 L 12 3 L 5 3 z M 14 3 L 14 5 L 17.585938 5 L 8.2929688 14.292969 L 9.7070312 15.707031 L 19 6.4140625 L 19 10 L 21 10 L 21 3 L 14 3 z"
+         />
+       </svg>; *)
+let render_svg () =
+  let path =
+    React.createElement "path"
+      [| React.Attribute.String
+           ( "d"
+           , "M 5 3 C 3.9069372 3 3 3.9069372 3 5 L 3 19 C 3 20.093063 \
+              3.9069372 21 5 21 L 19 21 C 20.093063 21 21 20.093063 21 19 L 21 \
+              12 L 19 12 L 19 19 L 5 19 L 5 5 L 12 5 L 12 3 L 5 3 z M 14 3 L \
+              14 5 L 17.585938 5 L 8.2929688 14.292969 L 9.7070312 15.707031 L \
+              19 6.4140625 L 19 10 L 21 10 L 21 3 L 14 3 z" )
+      |]
+      []
+  in
+  let svg =
+    React.createElement "svg"
+      [| React.Attribute.String ("xmlns", "http://www.w3.org/2000/svg")
+       ; React.Attribute.String ("viewBox", "0 0 24 24")
+       ; React.Attribute.String ("width", "24px")
+       ; React.Attribute.String ("height", "24px")
+      |]
+      [ path ]
+  in
+  assert_string
+    (ReactDOM.renderToStaticMarkup svg)
+    "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\" \
+     width=\"24px\" height=\"24px\"><path d=\"M 5 3 C 3.9069372 3 3 3.9069372 \
+     3 5 L 3 19 C 3 20.093063 3.9069372 21 5 21 L 19 21 C 20.093063 21 21 \
+     20.093063 21 19 L 21 12 L 19 12 L 19 19 L 5 19 L 5 5 L 12 5 L 12 3 L 5 3 \
+     z M 14 3 L 14 5 L 17.585938 5 L 8.2929688 14.292969 L 9.7070312 15.707031 \
+     L 19 6.4140625 L 19 10 L 21 10 L 21 3 L 14 3 z\"></path></svg>"
+
 let case title fn = Alcotest.test_case title `Quick fn
 
 let tests =
@@ -279,4 +319,5 @@ let tests =
     ; case "events" event
     ; case "_onclick" _onclick_render_as_string
     ; case "!DOCTYPE" render_with_doc_type
+    ; case "svg" render_svg
     ] )
