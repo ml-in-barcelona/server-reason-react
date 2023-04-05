1eed038a9e6d6c5d0d4a92218fb28aa30bf157be
David Sancho Moreno <dsnxmoreno@gmail.com>
2023-04-05 17:56:06 +0200

    Add Dom.*

--- lib/web/webapi.ml
+++ lib/web/webapi.ml
@@ -1,4 +1,22 @@
 module Dom = struct
-  type element
   type window
+  type document
+  type element
+  type eventTarget
+
+  module MouseEvent = struct
+    let target event = event#target
+  end
+
+  module Element = struct
+    let closest event : element = event#target
+  end
+
+  module EventTarget = struct
+    let contains (_target : element) = false
+
+    external unsafeAsDocument : eventTarget -> document = "%identity"
+    external unsafeAsElement : eventTarget -> element = "%identity"
+    external unsafeAsWindow : eventTarget -> window = "%identity"
+  end
 end
