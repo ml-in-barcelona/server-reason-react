65e99caa46c7300978846de9ea2a61ee752b9447
David Sancho Moreno <dsnxmoreno@gmail.com>
2023-05-14 22:15:14 +0200

    Impl Js.Console

--- lib/js/js.ml
+++ lib/js/js.ml
@@ -1,3 +1,4 @@
+(* type 'a t = <..> *)
 type 'a null = 'a option
 type 'a undefined = 'a option
 
@@ -40,7 +41,41 @@ module Exn = struct
 end
 
 module Console = struct
-  let error = print_endline
-  let log = print_endline
-  let warn = print_endline
+  let log a = print_endline (Obj.magic a)
+
+  let log2 a b =
+    print_endline (Printf.sprintf "%s %s" (Obj.magic a) (Obj.magic b))
+
+  let log3 a b c =
+    print_endline
+      (Printf.sprintf "%s %s %s" (Obj.magic a) (Obj.magic b) (Obj.magic c))
+
+  let log4 a b c d =
+    print_endline
+      (Printf.sprintf "%s %s %s %s" (Obj.magic a) (Obj.magic b) (Obj.magic c)
+         (Obj.magic d))
+
+  let logMany arr = Array.iter log arr
+  let info = log
+  let info2 = log2
+  let info3 = log3
+  let info4 = log4
+  let infoMany = logMany
+  let error = log
+  let error2 = log2
+  let error3 = log3
+  let error4 = log4
+  let errorMany = logMany
+  let warn = log
+  let warn2 = log2
+  let warn3 = log3
+  let warn4 = log4
+  let warnMany = logMany
+
+  (* external trace : unit -> unit = "trace" [@@bs.val] [@@bs.scope "console"] *)
+  (* external timeStart : string -> unit = "time" [@@bs.val] [@@bs.scope "console"] *)
+  (* external timeEnd : string -> unit = "timeEnd" [@@bs.val] [@@bs.scope "console"] *)
+  let trace () = ()
+  let timeStart _ = ()
+  let timeEnd _ = ()
 end
