28ed99bf6e81d84ddeec1273127c847df63fbf81
David Sancho Moreno <dsnxmoreno@gmail.com>
2023-06-26 16:05:24 +0200

    Remove previous webapi minimal

--- lib/react/react.ml
+++ lib/react/react.ml
@@ -1,8 +1,12 @@
 type domRef
 
+module Dom = struct
+  type element
+end
+
 module Ref = struct
   type t = domRef
-  type currentDomRef = Webapi.Dom.element Js.nullable ref
+  type currentDomRef = Dom.element Js.nullable ref
   type callbackDomRef
 
   external domRef : currentDomRef -> domRef = "%identity"
