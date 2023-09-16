fe08958069987023f730bd7dc37ceba35dde44eb
Joseph Price <pricejosephd@gmail.com>
2023-09-16 10:02:19 -0400

    add flake

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "server-reason-react";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs = {
+      url = "github:nix-ocaml/nix-overlays";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+    melange = {
+      url = "github:melange-re/melange";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , melange
+    }:
+    (flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages."${system}".appendOverlays [
+        melange.overlays.default
+        (self: super: {
+          ocamlPackages = super.ocaml-ng.ocamlPackages_5_1;
+        })
+      ];
+      inherit (pkgs) ocamlPackages;
+    in
+    with ocamlPackages;
+    rec {
+      devShells.default = pkgs.mkShell {
+        buildInputs = [
+          lwt
+          lwt_ppx
+          ocamlPackages.melange
+          ocaml_pcre
+          alcotest
+          alcotest-lwt
+          fmt
+          ppxlib
+        ];
+        nativeBuildInputs = [
+          findlib
+          ocaml
+          ocaml-lsp
+          reason
+          dune_3
+          reason-native.refmterr
+          ocamlformat
+        ];
+        OCAMLRUNPARAM = "b";
+      };
+    }));
+}
