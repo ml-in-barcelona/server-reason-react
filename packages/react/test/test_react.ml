7aff7339e6894df94f408f55fa5bf1900519a372
David Sancho Moreno <dsnxmoreno@gmail.com>
2024-04-22 12:30:51 +0200

    Make React.Children.* APIs work as expected

--- packages/react/test/test_react.ml
+++ packages/react/test/test_react.ml
@@ -1,3 +1,5 @@
+let test title fn = Alcotest.test_case title `Quick fn
+
 let assert_string left right =
   Alcotest.check Alcotest.string "should be equal" right left
 
@@ -24,11 +26,41 @@ let use_effect_doesnt_fire () =
   in
   assert_string (ReactDOM.renderToStaticMarkup app) "<div>foo</div>"
 
-let case title fn = Alcotest.test_case title `Quick fn
+module Gap = struct
+  let make ~children =
+    React.Children.map children (fun element ->
+        if element = React.null then React.null
+        else
+          React.createElement "div"
+            [ React.JSX.String ("class", "divider") ]
+            [ element ])
+end
+
+let children_map_one_element () =
+  let app =
+    React.Upper_case_component
+      (fun () -> Gap.make ~children:(React.string "foo"))
+  in
+  assert_string
+    (ReactDOM.renderToStaticMarkup app)
+    "<div class=\"divider\">foo</div>"
+
+let children_map_list_element () =
+  let app =
+    React.Upper_case_component
+      (fun () ->
+        Gap.make
+          ~children:(React.list [ React.string "foo"; React.string "lola" ]))
+  in
+  assert_string
+    (ReactDOM.renderToStaticMarkup app)
+    "<div class=\"divider\">foo</div><div class=\"divider\">lola</div>"
 
 let tests =
   ( "React",
     [
-      case "useState" use_state_doesnt_fire;
-      case "useEffect" use_effect_doesnt_fire;
+      test "useState" use_state_doesnt_fire;
+      test "useEffect" use_effect_doesnt_fire;
+      test "Children.map" children_map_one_element;
+      test "Children.map" children_map_list_element;
     ] )
