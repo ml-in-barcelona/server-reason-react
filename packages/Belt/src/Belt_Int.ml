6bcf0b7c9ae4a2c5d403d0fc43fcb10730b21d6d
David Sancho Moreno <dsnxmoreno@gmail.com>
2024-08-02 16:30:42 +0200

    Remove identities and externals from Belt.Int

--- packages/Belt/src/Belt_Int.ml
+++ packages/Belt/src/Belt_Int.ml
@@ -1,11 +1,8 @@
-external toFloat : int -> float = "%identity"
-external fromFloat : float -> int = "%intoffloat"
-
-let fromString = int_of_string
+let toFloat = Stdlib.float_of_int
+let fromFloat = Stdlib.int_of_float
 let fromString i = try Some (int_of_string i) with _ -> None
 let toString = string_of_int
-
-external ( + ) : int -> int -> int = "%addint"
-external ( - ) : int -> int -> int = "%subint"
-external ( * ) : int -> int -> int = "%mulint"
-external ( / ) : int -> int -> int = "%divint"
+let ( + ) = Stdlib.( + )
+let ( - ) = Stdlib.( - )
+let ( * ) = Stdlib.( * )
+let ( / ) = Stdlib.( / )
--- packages/Belt/src/Belt_Int.mli
+++ packages/Belt/src/Belt_Int.mli
@@ -1,9 +1,8 @@
-external toFloat : int -> float = "%identity"
-external fromFloat : float -> int = "%intoffloat"
-val fromString : string -> int
+val toFloat : int -> float
+val fromFloat : float -> int
 val fromString : string -> int option
 val toString : int -> string
-external ( + ) : int -> int -> int = "%addint"
-external ( - ) : int -> int -> int = "%subint"
-external ( * ) : int -> int -> int = "%mulint"
-external ( / ) : int -> int -> int = "%divint"
+val ( + ) : int -> int -> int
+val ( - ) : int -> int -> int
+val ( * ) : int -> int -> int
+val ( / ) : int -> int -> int
