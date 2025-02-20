b6fb0a65b186a8df3e7c589db5eb59100223b250
David Sancho Moreno <dsnxmoreno@gmail.com>
2025-02-20 19:27:31 +0100

    run esbuild once for all client files

--- packages/extract-client-components/esbuild-plugin.mjs
+++ packages/extract-client-components/esbuild-plugin.mjs
@@ -2,7 +2,7 @@ import Fs from "node:fs/promises";
 import Path from "node:path";
 import { execSync } from "node:child_process";
 
-async function generateBootstrapFile (output, content) {
+async function generateBootstrapFile(output, content) {
 	let previousContent = undefined;
 	try {
 		previousContent = await Fs.readFile(output, "utf8");
@@ -15,36 +15,63 @@ async function generateBootstrapFile (output, content) {
 	if (contentHasChanged) {
 		await Fs.writeFile(output, content, "utf8");
 	}
-};
+}
 
 export function plugin(config) {
 	return {
 		name: "extract-client-components",
 		setup(build) {
-			if (config.output && typeof config.output !== "string") {
-				console.error("output must be a string");
+			if (
+				config.bootstrapOutput &&
+				typeof config.bootstrapOutput !== "string"
+			) {
+				console.error("bootstrapOutput must be a string");
 				return;
 			}
-			const output = config.output || "./bootstrap.js";
+			const bootstrapOutput = config.bootstrapOutput || "./bootstrap.js";
+
+			if (
+				config.mockWebpackRequire &&
+				typeof config.mockWebpackRequire !== "boolean"
+			) {
+				console.error("mockWebpackRequire must be a boolean");
+				return;
+			}
+			const mockWebpackRequire = config.mockWebpackRequire || false;
+
+			if (mockWebpackRequire) {
+				let webpackRequireMock = `
+window.__webpack_require__ = window.__webpack_require__ || ((id) => {
+  const component = window.__client_manifest_map[id];
+  if (!component) {
+    throw new Error(\`Could not find client component with id: \${id}\`);
+  }
+  return { __esModule: true, default: component };
+});
+window.__client_manifest_map = window.__client_manifest_map || {};`;
+
+				build.initialOptions.banner = {
+					js: webpackRequireMock,
+				};
+			}
+
+			if (!config.target) {
+				console.error("target is required");
+				return;
+			}
+			if (typeof config.target !== "string") {
+				console.error("target must be a string");
+				return;
+			}
 
 			build.onStart(async () => {
-				if (!config.target) {
-					console.error("target is required");
-					return;
-				}
-				if (typeof config.target !== "string") {
-					console.error("target must be a string");
-					return;
-				}
-
-				const target = config.target;
 				try {
 					/* TODO: Make sure `server_reason_react.extract_client_components` is available in $PATH */
 					const bootstrapContent = execSync(
-						`server_reason_react.extract_client_components ${target}`,
+						`server_reason_react.extract_client_components ${config.target}`,
 						{ encoding: "utf8" },
 					);
-					await generateBootstrapFile(output, bootstrapContent);
+					await generateBootstrapFile(bootstrapOutput, bootstrapContent);
 				} catch (e) {
 					console.log("Extraction of client components failed:");
 					console.error(e);
@@ -64,26 +91,16 @@ export function plugin(config) {
 				return null;
 			});
 
-			let webpackRequireMock = `
-window.__webpack_require__ = window.__webpack_require__ || ((id) => {
-  const component = window.__client_manifest_map[id];
-  if (!component) {
-    throw new Error(\`Could not find client component with id: \${id}\`);
-  }
-  return { __esModule: true, default: component };
-});
-window.__client_manifest_map = window.__client_manifest_map || {};`;
-
-			build.initialOptions.banner = {
-				js: webpackRequireMock,
-			};
-
 			build.onLoad({ filter: /.*/, namespace: "entrypoint" }, async (args) => {
 				const filePath = args.path.replace(/^entrypoint:/, "");
 				const entryPointContents = await Fs.readFile(filePath, "utf8");
+				const relativeBootstrapOutput = Path.relative(
+					Path.dirname(filePath),
+					bootstrapOutput,
+				);
 
 				const contents = `
-require("${output}");
+require("./${relativeBootstrapOutput}");
 ${entryPointContents}`;
 
 				return {
