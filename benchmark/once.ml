7155e3df63861f2bf3105ab70281b1b2c142ef0f
David Sancho Moreno <dsnxmoreno@gmail.com>
2025-02-20 12:34:47 +0100

    Add more benchone

--- benchmark/once.ml
+++ benchmark/once.ml
@@ -47,14 +47,14 @@ let main () =
     ()
   in
 
-  let render_React_list () =
-    let list = React.list (List.init 50 (fun i -> React.string (Printf.sprintf "index: %d" i))) in
+  let render_React_list ~num () =
+    let list = React.list (List.init num (fun i -> React.string (Printf.sprintf "index: %d" i))) in
     let _ = ReactDOM.renderToStaticMarkup (React.createElement "div" [] [ list ]) in
     ()
   in
 
-  let render_React_array () =
-    let array = React.array (Array.init 50 (fun i -> React.string (Printf.sprintf "index: %d" i))) in
+  let render_React_array ~num () =
+    let array = React.array (Array.init num (fun i -> React.string (Printf.sprintf "index: %d" i))) in
     let _ = ReactDOM.renderToStaticMarkup (React.createElement "div" [] [ array ]) in
     ()
   in
@@ -63,8 +63,12 @@ let main () =
   measure_alloc "Use list direct style" (fun () -> loop 10000 direct_style);
   measure_alloc "Render <HelloWorld />" (fun () -> loop 10000 render_hello_world);
   measure_alloc "Render <App />" (fun () -> loop 10000 render_app);
-  measure_alloc "Render React.list" (fun () -> loop 10000 render_React_list);
-  measure_alloc "Render React.array" (fun () -> loop 10000 render_React_array);
+  measure_alloc "Render React.list 10" (fun () -> loop 10000 (render_React_list ~num:10));
+  measure_alloc "Render React.array 10" (fun () -> loop 10000 (render_React_array ~num:10));
+  measure_alloc "Render React.list 100" (fun () -> loop 10000 (render_React_list ~num:100));
+  measure_alloc "Render React.array 100" (fun () -> loop 10000 (render_React_array ~num:100));
+  (* measure_alloc "Render React.list 1000" (fun () -> loop 10000 (render_React_list ~num:1000)); *)
+  (* measure_alloc "Render React.array 1000" (fun () -> loop 10000 (render_React_array ~num:1000)); *)
   Lwt.return ()
 
 let () = Lwt_main.run (main ())
