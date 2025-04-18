106e6a18fff1e68a77eb42c66a383219bc03c839
David Sancho Moreno <dsnxmoreno@gmail.com>
2025-04-18 17:02:20 +0200

    Remove useless comment in tests

--- packages/reactDom/src/ReactServerDOM.ml
+++ packages/reactDom/src/ReactServerDOM.ml
@@ -277,11 +277,9 @@ srr_stream.close = () => {
   srr_stream._c.close();
 };
 srr_stream.readable_stream = new ReadableStream({ start(c) { srr_stream._c = c; } });
-        |};
+|};
     ]
 
-(* Copied from ReactDOM.ml, TODO: Move somewhere common? *)
-(* https://github.com/facebook/react/blob/493f72b0a7111b601c16b8ad8bc2649d82c184a0/packages/react-dom-bindings/src/server/fizz-instruction-set/ReactDOMFizzInstructionSetShared.js#L46 *)
 let rc_function_definition =
   {|function $RC(a,b){a=document.getElementById(a);b=document.getElementById(b);b.parentNode.removeChild(b);if(a){a=a.previousSibling;var f=a.parentNode,c=a.nextSibling,e=0;do{if(c&&8===c.nodeType){var d=c.data;if("/$"===d)if(0===e)break;else e--;else"$"!==d&&"$?"!==d&&"$!"!==d||e++}d=c.nextSibling;f.removeChild(c);c=d}while(c);for(;b.firstChild;)f.insertBefore(b.firstChild,c);a.data="$";a._reactRetry&&a._reactRetry()}}|}
 
