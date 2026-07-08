7cb72d014746f9f53414455cbe14d06a125c9aa4
David Sancho <dsnxmoreno@gmail.com>
2026-07-08 12:16:41 +0200

    Route remaining Belt/Js number sites through quickjs (#372)
    * Route Belt.Float toString/fromString through quickjs
    
    * Route Belt.Int.fromString through quickjs parseInt
    
    * Match JS Number() semantics in Js.Float.fromString
    
    * Return floats beyond int range from Js.Global.parseInt
    
    * Fix truncated Js.Math._SQRT2 constant
    
    * Remove redundant comments in Js.Global parseInt/parseFloat
    
    * Allow GITHUB_TOKEN to comment benchmark results on PRs
    
    * Constrain melange to < 7.0.0
    
    melange 7 (released 2026-06-22) inlines externals cross-module with
    --cross-module-opt, which copies relative [@mel.module "./..."] paths
    into consumer modules where they no longer resolve. This broke the demo
    esbuild step in the Docker image build (Could not resolve
    "./ReactServerDOMEsbuild.js").

--- packages/Belt/src/Belt_Float.ml
+++ packages/Belt/src/Belt_Float.ml
@@ -1,12 +1,15 @@
 let toInt = Stdlib.int_of_float
 let fromInt = Stdlib.float_of_int
-let fromString i = try Some (float_of_string i) with _ -> None
 
-let toString value =
-  let string = Stdlib.string_of_float value in
-  let length = String.length string in
-  if length > 0 && string.[length - 1] = '.' then String.sub string 0 (length - 1) else string
+(* JavaScript parseFloat semantics via quickjs: skips leading whitespace and
+   parses the longest numeric prefix ("3.5px" is Some 3.5, "0x10" is Some 0.).
+   None plays the role of JavaScript's NaN. *)
+let fromString i = Quickjs.Global.parse_float i
 
+(* JavaScript String(number) semantics via quickjs: shortest round-trip
+   representation, so 0.1 +. 0.2 is "0.30000000000000004", nan is "NaN" and
+   infinity is "Infinity". *)
+let toString value = Quickjs.Number.Prototype.to_string value
 let ( + ) = Stdlib.( +. )
 let ( - ) = Stdlib.( -. )
 let ( * ) = Stdlib.( *. )
