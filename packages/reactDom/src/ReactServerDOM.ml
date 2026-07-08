94b6a9749a0758a6ccad29980d529354bdd61674
David Sancho Moreno <david.sancho@ahrefs.com>
2026-07-08 16:50:27 +0000

    Share the vendored fizz instruction set between ReactDOM and ReactServerDOM

--- packages/reactDom/src/ReactServerDOM.ml
+++ packages/reactDom/src/ReactServerDOM.ml
@@ -929,14 +929,9 @@ srr_stream.readable_stream = new ReadableStream({ start(c) { srr_stream._c = c; 
 |};
     ]
 
-let rc_function_definition =
-  {|function $RC(a,b){a=document.getElementById(a);b=document.getElementById(b);b.parentNode.removeChild(b);if(a){a=a.previousSibling;var f=a.parentNode,c=a.nextSibling,e=0;do{if(c&&8===c.nodeType){var d=c.data;if("/$"===d)if(0===e)break;else e--;else"$"!==d&&"$?"!==d&&"$!"!==d||e++}d=c.nextSibling;f.removeChild(c);c=d}while(c);for(;b.firstChild;)f.insertBefore(b.firstChild,c);a.data="$";a._reactRetry&&a._reactRetry()}}|}
-
+let rc_function_definition = Fizz_instructions.complete_boundary
 let rc_function_script = Html.node "script" [] [ Html.raw rc_function_definition ]
-
-(* https://github.com/facebook/react/blob/493f72b0a7111b601c16b8ad8bc2649d82c184a0/packages/react-dom-bindings/src/server/fizz-instruction-set/ReactDOMFizzInstructionSetShared.js#L127 *)
-let rx_function_definition =
-  {|$RX=function(b,c,d,e,f){var a=document.getElementById(b);a&&(b=a.previousSibling,b.data="$!",a=a.dataset,c&&(a.dgst=c),d&&(a.msg=d),e&&(a.stck=e),f&&(a.cstck=f),b._reactRetry&&b._reactRetry())};|}
+let rx_function_definition = Fizz_instructions.client_render_boundary
 
 let timeout_error_message =
   "Switched to client rendering because the server rendering aborted due to:\n\nThe render timed out."
